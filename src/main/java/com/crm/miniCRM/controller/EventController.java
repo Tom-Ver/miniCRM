7caaf0404b90c7cc6abf10dd136326c0e08c4295
Tom Vervaeren <tom.vervaeren@student.ucll.be>
2022-05-18 20:37:48 +0200

    EventController

---
+++ src/main/java/com/crm/miniCRM/controller/EventController.java
@@ -0,0 +1,67 @@
+package com.crm.miniCRM.controller;
+
+import com.crm.miniCRM.model.Address;
+import com.crm.miniCRM.model.Community;
+import com.crm.miniCRM.model.Event;
+import com.crm.miniCRM.model.persistence.AddressRepository;
+import com.crm.miniCRM.model.persistence.EventRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping(value="/events")
+public class EventController {
+
+    private EventRepository eventService;
+
+    public EventController(EventRepository eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping
+    public String getEvents(Model model) {
+        Iterable<Event> addresses = eventService.findAll();
+        List<EventDto> eventDtos = new ArrayList<>();
+        addresses.forEach(p-> eventDtos.add(convertToDto(p)));
+        model.addAttribute("events", eventDtos);
+        return "events";
+    }
+
+    @GetMapping("/new")
+    public String newAddress(Model model) {
+        model.addAttribute("events", new EventDto());
+        return "new-event";
+    }
+
+    @PostMapping
+    public String addAddress(EventDto event) {
+        eventService.save(convertToEntity(event));
+
+        return "redirect:/events";
+    }
+
+    protected EventDto convertToDto(Event entity) {
+        EventDto dto = new EventDto(
+                entity.getId(),
+                entity.getDescription(),
+                entity.getDate());
+        return dto;
+    }
+
+    protected Event convertToEntity(EventDto dto) {
+        Event event = new Event(dto.getDescription(),
+                dto.getDate());
+        if (!StringUtils.isEmpty(dto.getId())) {
+            event.setId(dto.getId());
+        }
+        return event;
+    }
+
+}
