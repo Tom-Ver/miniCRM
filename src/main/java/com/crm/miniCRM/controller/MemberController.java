c4e6dbf06585f46da17c3f8cff46102e8cc1eb9e
Sennaxis <92334921+Sennaxis@users.noreply.github.com>
2022-05-26 12:43:44 +0200

    members update

--- src/main/java/com/crm/miniCRM/controller/MemberController.java
+++ src/main/java/com/crm/miniCRM/controller/MemberController.java
@@ -2,12 +2,17 @@ package com.crm.miniCRM.controller;
 
 
 import com.crm.miniCRM.dto.MemberDto;
+import com.crm.miniCRM.model.Community;
 import com.crm.miniCRM.model.Member;
+import com.crm.miniCRM.model.Person;
+import com.crm.miniCRM.model.persistence.CommunityRepository;
 import com.crm.miniCRM.model.persistence.MemberID;
 import com.crm.miniCRM.model.persistence.MemberRepository;
+import com.crm.miniCRM.model.persistence.PersonRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,13 +23,17 @@ import java.util.List;
 @RequestMapping(value = "/members")
 public class MemberController {
     private MemberRepository memberService;
+    private PersonRepository personService;
+    private CommunityRepository communityService;
 
-    public MemberController(MemberRepository memberService) {
+    public MemberController(MemberRepository memberService, PersonRepository personService, CommunityRepository communityService) {
         this.memberService = memberService;
+        this.personService = personService;
+        this.communityService = communityService;
     }
 
     @GetMapping
-    public String getMembers(Model model) {
+    public String getMembers(Model model) { //should display names instead of id's, todo
         Iterable<Member> members = memberService.findAll();
         List<MemberDto> memberDtos = new ArrayList<>();
         members.forEach(m-> memberDtos.add(convertToDto(m)));
@@ -32,10 +41,33 @@ public class MemberController {
         return "members";
     }
 
+    @GetMapping (value = "/by-community/{id}")
+    public String getMembersByIdCommunity_ID(Model model, @PathVariable Long id) {
+        Iterable<Member> members = memberService.findAllByIdCommunity_ID(id);
+        List<MemberDto> memberDtos = new ArrayList<>();
+        members.forEach(m-> memberDtos.add(convertToDto(m)));
+        model.addAttribute("members", memberDtos);
+        return "members-by-community";
+    }
+
     @GetMapping("/new")
     public String newMember(Model model) {
         model.addAttribute("member", new MemberDto());
+        List<Person> personList = (List<Person>) personService.findAll();
+        model.addAttribute("personList", personList);
+        List<Community> communityList = (List<Community>) communityService.findAll();
+        model.addAttribute("communityList", communityList);
         return "new-member";
+    }
+
+    @GetMapping("/new-by-community/{id}")
+    public String newMember(Model model, @PathVariable Long id) {
+        MemberDto newMember = new MemberDto();
+        newMember.setCommunity_ID(id);
+        model.addAttribute("member", newMember);
+        List<Person> personList = (List<Person>) personService.findAll();
+        model.addAttribute("personList", personList);
+        return "new-member-by-community";
     }
 
     @PostMapping
@@ -43,6 +75,13 @@ public class MemberController {
         memberService.save(convertToEntity(member));
 
         return "redirect:/members";
+    }
+
+    @PostMapping("/{id}")
+    public String addMemberToCommunity(MemberDto member) {  //not working, todo
+        memberService.save(convertToEntity(member));
+
+        return "redirect:/members-by-community";
     }
 
     protected MemberDto convertToDto(Member entity) {
--- src/main/java/com/crm/miniCRM/model/persistence/MemberRepository.java
+++ src/main/java/com/crm/miniCRM/model/persistence/MemberRepository.java
@@ -1,12 +1,17 @@
 package com.crm.miniCRM.model.persistence;
 
 import com.crm.miniCRM.model.Member;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
 import java.util.Optional;
 
 public interface MemberRepository extends CrudRepository<Member, MemberID> {
 
 
     Optional<Member> findById(MemberID id);
+
+    @Query("select m from Member m where m.Id.community_ID = ?1")
+    Collection<Member> findAllByIdCommunity_ID(Long id);
 }
